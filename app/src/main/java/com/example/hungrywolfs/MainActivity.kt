5ff16b90243d53fc3fef0b0682f253e9e6eb53ce
DESKTOP-N47V2B1\Lidia <sav.betuel1999@gmail.com>
2021-08-09 22:10:55 +0300

    Splash screen

--- app/src/main/java/com/example/hungrywolfs/MainActivity.kt
+++ app/src/main/java/com/example/hungrywolfs/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.hungrywolfs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //sleep(1000)
+
+        // set the default theme
+        setTheme(R.style.Theme_HungryWolfs)
         setContentView(R.layout.activity_main)
     }
 }
