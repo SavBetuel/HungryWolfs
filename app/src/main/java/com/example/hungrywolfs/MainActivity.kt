56f47667341f2cec69558ed4186e5097a4150275
DESKTOP-N47V2B1\Lidia <sav.betuel1999@gmail.com>
2021-08-10 14:32:26 +0300

    Home screen with FragmentContainerView and BottomNavigationView.

--- app/src/main/java/com/example/hungrywolfs/MainActivity.kt
+++ app/src/main/java/com/example/hungrywolfs/MainActivity.kt
@@ -2,12 +2,29 @@ package com.example.hungrywolfs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setTheme(R.style.Theme_HungryWolfs)
+
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
