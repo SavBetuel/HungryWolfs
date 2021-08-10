f552391691f6ab9f28116c1ece6e1e9d1cff5c50
DESKTOP-N47V2B1\Lidia <sav.betuel1999@gmail.com>
2021-08-10 10:55:10 +0300

    Remove comments

--- app/src/main/java/com/example/hungrywolfs/MainActivity.kt
+++ app/src/main/java/com/example/hungrywolfs/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.hungrywolfs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //sleep(1000)
 
-        // set the default theme
         setTheme(R.style.Theme_HungryWolfs)
         setContentView(R.layout.activity_main)
     }
