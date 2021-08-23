04acc1e3b5edcd924f74c047bcc4b12ea73491fd
DESKTOP-N47V2B1\Lidia <sav.betuel1999@gmail.com>
2021-08-23 11:42:12 +0300

    Profile design and functionality

--- app/src/main/java/com/example/hungrywolfs/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/hungrywolfs/fragments/ProfileFragment.kt
@@ -1,25 +1,55 @@
 package com.example.hungrywolfs.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.hungrywolfs.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.hungrywolfs.databinding.FragmentProfileBinding
+import com.example.hungrywolfs.model.ProfileViewModel
+
+
+const val TERMS_AND_CONDITIONS_URI= "https://www.wolfpack-digital.com/privacy"
 
 class ProfileFragment : Fragment() {
+
+    lateinit var binding: FragmentProfileBinding
+    private val viewModel: ProfileViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel=viewModel
 
+        setupObservers()
+    }
+
+    private fun setupObservers(){
+        viewModel.navigateBack.observe(viewLifecycleOwner){
+            findNavController().popBackStack()
+        }
+
+        viewModel.navigateFavourites.observe(viewLifecycleOwner){
+            findNavController().navigate(R.id.action_profileFragment_to_favouritesFragment)
+        }
+
+        viewModel.navigateTermsAndConditions.observe(viewLifecycleOwner){
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(TERMS_AND_CONDITIONS_URI))
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/hungrywolfs/network/UserData.kt
@@ -0,0 +1,3 @@
+package com.example.hungrywolfs.network
+
+data class UserData(val name: String, val email: String, val phone: String)
