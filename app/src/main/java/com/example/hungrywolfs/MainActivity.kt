324b63ad4712efbaa0626b20392beea856386ab8
DESKTOP-N47V2B1\Lidia <sav.betuel1999@gmail.com>
2021-08-26 07:45:09 +0300

    Simplify the internet connection check

--- app/src/main/java/com/example/hungrywolfs/MainActivity.kt
+++ app/src/main/java/com/example/hungrywolfs/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.hungrywolfs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.hungrywolfs.databinding.ActivityMainBinding
+import com.example.hungrywolfs.fragments.NoInternetConnectionFragment
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
-    lateinit var connectionLiveData: ConnectionLiveData
 
     private val destinationChangedListener = NavController.OnDestinationChangedListener{_, destination, _ ->
         val bottomNavVisibility = when (destination.id) {
@@ -24,6 +35,7 @@ class MainActivity : AppCompatActivity() {
             if (bottomNavVisibility) View.VISIBLE else View.GONE
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -34,16 +46,35 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNavigationView.setupWithNavController(navController)
 
-        connectionLiveData = ConnectionLiveData(this)
-        connectionLiveData.observe(this) {
-            if(it){
-                if( findNavController(R.id.fragmentContainerView).currentBackStackEntry?.destination?.id == R.id.noInternetConnectionFragment)
-                    findNavController(R.id.fragmentContainerView).popBackStack()
-            }
-            else {
-              findNavController(R.id.fragmentContainerView).navigate(R.id.noInternetConnectionFragment)
+        fixedRateTimer("timer", false, 0, 1000) {
+           runOnUiThread {
+               checkConnection()
             }
         }
+    }
 
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun checkConnection(){
+        if(isOnline()){
+            if( findNavController(R.id.fragmentContainerView).currentBackStackEntry?.destination?.id == R.id.noInternetConnectionFragment)
+                findNavController(R.id.fragmentContainerView).popBackStack()
+        }
+        else {
+            findNavController(R.id.fragmentContainerView).navigate(R.id.noInternetConnectionFragment)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun isOnline(): Boolean {
+        val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = cm.activeNetwork
+        val activeNetwork = cm.getNetworkCapabilities(networkCapabilities)
+
+        return when {
+            activeNetwork?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true -> true
+            activeNetwork?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true -> true
+            else -> false
+        }
     }
 }
--- app/src/main/java/com/example/hungrywolfs/fragments/NoInternetConnectionFragment.kt
+++ app/src/main/java/com/example/hungrywolfs/fragments/NoInternetConnectionFragment.kt
@@ -1,11 +1,11 @@
 package com.example.hungrywolfs.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.hungrywolfs.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.hungrywolfs.databinding.FragmentNoInternetConnectionBinding
 
 
@@ -24,6 +24,9 @@ class NoInternetConnectionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.tryAgain.setOnClickListener {
+            Toast.makeText(context, "Trying to connect...", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
